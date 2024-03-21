b6812c3f35ed1cea820676beb4481152166a823d
강영규 <skj45691@gmail.com>
2024-03-21 18:18:26 +0900

    Update server.js

--- server.js
+++ server.js
@@ -13,9 +13,7 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 // MongoDB 연결
-const mongoURI = process.env.mongodbURI;
-console.log(mongoURI);
-mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(mongodbURI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log('MongoDB에 연결되었습니다.'))
     .catch(err => console.error('MongoDB 연결 오류:', err));
 
