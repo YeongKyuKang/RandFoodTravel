25468f092311530a3fe0e8e05164f3b8963392d9
YeongKyuKang <skj45691@gmail.com>
2024-11-15 10:52:28 +0900

    Update server.js

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const PORT = process.env.PORT || 3000;
 
 // MongoDB 연결
 const mongoURI = process.env.MONGODBURI;
-mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(mongoURI)
     .then(() => console.log('MongoDB에 연결되었습니다.'))
     .catch(err => console.error('MongoDB 연결 오류:', err));
 // 미들웨어 설정
